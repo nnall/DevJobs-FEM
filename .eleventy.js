f9e6eff5b4a349c1b0652b66e699142da783fb35
nnall <83522890+nnall@users.noreply.github.com>
2023-04-15 18:19:46 -0500

    DevJobs2 initial commit

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+
+const sass = require("sass");
+
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('./src/assets');
+    eleventyConfig.addPassthroughCopy('./src/main.scss');
+    // eleventyConfig.addPassthroughCopy('./src/main.scss');
+    // eleventyConfig.addPassthroughCopy('./src/main.scss');
+
+
+    eleventyConfig.addTemplateFormats("scss");
+    // Creates the extension for use
+    eleventyConfig.addExtension("scss", {
+        outputFileExtension: "css", // optional, default: "html"
+
+        // `compile` is called once per .scss file in the input directory
+        compile: async function(inputContent) {
+        let result = sass.compileString(inputContent);
+
+        // This is the render function, `data` is the full data cascade
+        return async (data) => {
+            return result.css;
+        };
+        }
+    });
+
+    
+    
+
+    // Return your Object options:
+    return {
+      dir: {
+        input: "src",
+        output: "_site"
+      }
+    }
+  };
