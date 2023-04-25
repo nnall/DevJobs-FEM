5b81f677d69b8036506e8f9c60e32df0b52f219d
nnall <83522890+nnall@users.noreply.github.com>
2023-04-25 09:57:45 -0500

    for loop w/ _data .json file

--- .eleventy.js
+++ .eleventy.js
@@ -30,10 +30,7 @@ module.exports = function(eleventyConfig) {
 
     // Return your Object options:
     return {
-      markdownTemplateEngine: 'njk',
-      dataTemplateEngine: 'njk',
-      htmlTemplateEngine: 'njk',
-
+      
       dir: {
         input: "src",
         output: "_site"
