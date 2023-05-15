6e67a46063567c49b325c429ff9d05e12785044b
nnall <83522890+nnall@users.noreply.github.com>
2023-05-15 14:20:55 -0500

    final commit 2

--- .eleventy.js
+++ .eleventy.js
@@ -16,7 +16,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets/");
     eleventyConfig.addPassthroughCopy("./src/index.js");
     eleventyConfig.addPassthroughCopy("./src/job.js");
-    eleventyConfig.addPassthroughCopy("./src/forms.js");
     eleventyConfig.addPassthroughCopy("./src/changeTheme.js");
     
 
